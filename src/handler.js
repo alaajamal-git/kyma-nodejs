c0ba07674bfda572776aa7db350a727a01b1b808
I555107 <alaa.jamal@sap.com>
2022-10-04 21:25:15 +0200

    hello world!

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 module.exports = {
     main: async function (event, context) {
-      const message = `Hello World`
+      const message = `Hello World 22`
         + ` from the Kyma Function ${context["function-name"]}`
         + ` running on ${context.runtime}!`;
       console.log(message);
